f25af812954ad19af0e2bfc135a87ebb1c8e8c1d
Karinnaor <109036074+Karinnaor@users.noreply.github.com>
2022-09-19 12:49:26 +0300

    added bootstrap and ran an npm update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
+import React from "react";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Weather Search </h1>
-      </header>
-      <footer>
-        This project wad coded by Karin Naor, and is{" "}
-        <a
-          href="https://github.com/Karinnaor/karins-react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1> Weather Search </h1>
+          <button className="btn btn-primary"> Yes? </button>
+        </header>
+        <footer>
+          This project wad coded by Karin Naor, and is{" "}
+          <a
+            href="https://github.com/Karinnaor/karins-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
