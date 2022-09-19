c99e3ca3aa632252f0fcbd092c0a3cce95919d3d
Karinnaor <109036074+Karinnaor@users.noreply.github.com>
2022-09-19 11:01:55 +0300

    Deleted unnecessary code to begin my weather app fresh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Weather Search </h1>
       </header>
+      <footer>
+        This project wad coded by Karin Naor, and is{" "}
+        <a href="" target="_blank">
+          {" "}
+          open sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
