f486384c51c6ecd936e8527e67b87fc5cca956f4
Karinnaor <109036074+Karinnaor@users.noreply.github.com>
2022-09-22 22:37:58 +0300

    added option to convert temp from C to F and back

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeathrtInfo(props) {
   return (
@@ -21,10 +22,7 @@ export default function WeathrtInfo(props) {
             </div>
 
             <div className="float-left">
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="units"> °C</span>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
